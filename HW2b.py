a6ed09855424b58fd4fbc2b6683768d475c64766
AngLi929 <ang.li@yale.edu>
2018-01-28 23:07:23 -0500

    HW2

--- HW2b.py
+++ HW2b.py
@@ -2,3 +2,6 @@ import HW2 as hui
 
 print("score for", 1, 1, 1, 1, 2, 1, 1, 1)
 print(hui.get_score(1, 1, 1, 1, 2, 1, 1, 1))
+
+print("score for", 1, 1, 1, 1, 7, 1, 1, 1)
+print(hui.get_score(1, 1, 1, 1, 7, 1, 1, 1))
